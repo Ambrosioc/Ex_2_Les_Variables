62c0565aa49268428750ba06b845de1a69d5b123
Ambrosie Cazimira <a.cazimira@gmail.com>
2021-11-09 17:59:38 +0100

    Exercice non fini

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello")
+// Exercice 2 : Afficher les variable
+// Le bute de l'exercice est de trouver une solution pour afficher les variables avec le genre(H/F)
+// Ne pas oublié de verifier avec la commande "node main.js" dans le terminal
+
+
+var nom = "Ambro"; // On assigne la variables nom par une chaine de caratere
+var age = 29; // On assigne l'age avec un entier
+var man = true; // On assigne la variable avec un booléen (true/false)
+
+
+console.log("Nom : " + nom); // Affichage sur la console
+console.log("Age : " + age); // Affichage sur la console
+
+if(man === true){ // Ici
+    console.log("Sexe : Homme");
+}else{
+    console.log("Sexe : Femme")
+};
+
